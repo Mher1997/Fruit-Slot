16345f11331c5dcac7cac9237ddce4b22ee9e927
mher.garanyan <mher.garanyan@digitain.com>
2022-12-06 13:24:40 +0400

    first commit

---
+++ docs/main.410a7f74a9458559e524.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdateweb_app"]("main",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("e4593f7d11ef06303fcc")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=main.410a7f74a9458559e524.hot-update.js.map
---
+++ docs/main.63e3c688e31e80d611f3.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdateweb_app"]("main",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("410a7f74a9458559e524")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=main.63e3c688e31e80d611f3.hot-update.js.map
